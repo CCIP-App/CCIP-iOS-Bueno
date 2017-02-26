6cd10eba9a5cfd2ca30ce91a61ba79609d06ae7a
yckao <yckaokarl@gmail.com>
2017-02-27 05:16:40 +0800

    Add Telegram Support.

---
+++ CCIP-iOS-Bueno/CCIP-iOS-Bueno/MainTabBarController.m
@@ -0,0 +1,49 @@
+//
+//  MainTabBarController.m
+//  CCIP-iOS-Bueno
+//
+//  Created by 高宜誠 on 2017/2/27.
+//  Copyright © 2017年 高宜誠. All rights reserved.
+//
+
+#import "MainTabBarController.h"
+#import "TelegramViewController.h"
+@interface MainTabBarController ()
+
+@end
+
+@implementation MainTabBarController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.delegate = self;
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (BOOL)tabBarController:(UITabBarController *)tabBarController shouldSelectViewController:(UIViewController *)viewController {
+    if([viewController class] == [TelegramViewController class]) {
+        NSURL *telegramURL = [NSURL URLWithString:@"telegram://SITCONgeneral"];
+        if(![[UIApplication sharedApplication] canOpenURL:telegramURL])
+            telegramURL = [NSURL URLWithString:@"https://telegram.me/SITCONgeneral"];
+        [[UIApplication sharedApplication] openURL:telegramURL options:@{} completionHandler:nil];
+        return false;
+    }
+    return true;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ CCIP-iOS-Bueno/CCIP-iOS-Bueno/TelegramViewController.h
@@ -0,0 +1,13 @@
+//
+//  TelegramViewController.h
+//  CCIP-iOS-Bueno
+//
+//  Created by 高宜誠 on 2017/2/27.
+//  Copyright © 2017年 高宜誠. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TelegramViewController : UIViewController
+
+@end
