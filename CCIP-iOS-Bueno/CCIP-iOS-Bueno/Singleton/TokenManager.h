c16409aff3eea4f9036aa15064e6bb69e4c7ac73
yckao <yckaokarl@gmail.com>
2017-02-27 02:57:24 +0800

    Lots of change

---
+++ CCIP-iOS-Bueno/CCIP-iOS-Bueno/Singleton/TokenManager.h
@@ -0,0 +1,13 @@
+//
+//  TokenManager.h
+//  CCIP-iOS-Bueno
+//
+//  Created by 高宜誠 on 2017/2/26.
+//  Copyright © 2017年 高宜誠. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TokenManager : NSObject
+
+@end
---
+++ CCIP-iOS-Bueno/CCIP-iOS-Bueno/Singleton/TokenManager.m
@@ -0,0 +1,13 @@
+//
+//  TokenManager.m
+//  CCIP-iOS-Bueno
+//
+//  Created by 高宜誠 on 2017/2/26.
+//  Copyright © 2017年 高宜誠. All rights reserved.
+//
+
+#import "TokenManager.h"
+
+@implementation TokenManager
+
+@end
